993ea1f6c62716e94a2f33a8a87982020642c795
Laure Lozano <laurelozano@gmail.com>
2022-11-25 14:24:41 +0100

    script.js updated and done

---
+++ front/js/script.js
@@ -0,0 +1,23 @@
+let itemsSection = document.querySelector(".items");
+
+// Récupération des données de l'API
+fetch(`http://localhost:3000/api/products`)
+  .then((products) => products.json())
+  .then((products) => {
+    displayProducts(products);
+  });
+
+//Création des éléments + rajout des données dans les balises
+function displayProducts(products) {
+  itemsSection.innerHTML = "";
+  for (let i = 0; i < products.length; i+=1) {
+    itemsSection.innerHTML += `
+    <a href="./product.html?_id=${products[i]._id}"> 
+      <article>
+        <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
+        <h3 class="productName">${products[i].name}</h3> 
+        <p class="productDescription">${products[i].description}</p>
+      </article>
+    </a>`;
+  }
+}
