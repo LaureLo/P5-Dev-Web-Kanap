a956c842731573afbd9d7917c3a80d4cfcd89f7b
Laure Lozano <laurelozano@gmail.com>
2022-12-07 16:00:37 +0100

    refactoring product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,10 @@
-// Récuperer l'id du produit
-let product_id = new URLSearchParams(window.location.search).get("_id");
+// Récupérer l'id du produit
+const queryString = window.location.search 
+const urlParams = new URLSearchParams(queryString)
+const product_id = urlParams.get("_id");
 
-// Récupérer la donnée du produit grâce à son id
-fetch(`http://localhost:3000/api/products/` + product_id)
+// Récupérer les données du produit grâce à son id
+fetch(`http://localhost:3000/api/products/${product_id}`)
   .then((product) => product.json()) 
   .then((product) => {
     displayProductInfos(product);
@@ -11,20 +13,20 @@ fetch(`http://localhost:3000/api/products/` + product_id)
   });
 
 // Initialisation de l'object product_client
-let product_client = { 
+const product_client = { 
   id : product_id,
   color : "",
   quantity : 0
 };
 
 // Déclaration des selectors
-let product_img = document.querySelector(".item__img");
-let product_title = document.querySelector("#title");
-let product_price = document.querySelector("#price");
-let product_description = document.querySelector("#description");
-let product_colors = document.querySelector("#colors");
-let product_nb = document.querySelector("#quantity");
-let color_miss = document.querySelector(".item__content");
+const product_img = document.querySelector(".item__img");
+const product_title = document.querySelector("#title");
+const product_price = document.querySelector("#price");
+const product_description = document.querySelector("#description");
+const product_colors = document.querySelector("#colors");
+const product_nb = document.querySelector("#quantity");
+const color_miss = document.querySelector(".item__content");
 
 
 // Afficher les informations du produit avec une boucle for pour les couleurs
@@ -39,10 +41,10 @@ function displayProductInfos(product) {
 }
 //------------------------------------------------------------
 
-// Récuperer la valeur de couleur quand celle ci-change
+// Récupérer la valeur de couleur quand elle change
 function listenColorsEvent() {
-  product_colors.addEventListener("change", (event) => {
-    product_client.color = event.target.value;
+  product_colors.addEventListener("change", (e) => {
+    product_client.color = e.target.value;
     if(product_client.color != 0){
       document.querySelector(".color__miss").textContent = "";
     }
@@ -52,8 +54,8 @@ function listenColorsEvent() {
 
 // Récupération de la value de quantité quand elle change
 function listenQuantityEvent() {
-  product_nb.addEventListener("change", (event) => {
-    product_client.quantity = parseInt(event.target.value);
+  product_nb.addEventListener("change", (e) => {
+    product_client.quantity = parseInt(e.target.value);
     if(product_client.quantity != 0){
       document.querySelector(".quantity__miss").textContent = "";
     }
@@ -62,7 +64,7 @@ function listenQuantityEvent() {
 //------------------------------------------------------------
 
 // Click sur le bouton ajouter au panier
-let add_cart = document.querySelector("#addToCart");
+const add_cart = document.querySelector("#addToCart");
 add_cart.addEventListener("click", () => {
   verifyInput(product_client);
 });
@@ -86,12 +88,12 @@ function verifyInput(product_client) {
 }
 //------------------------------------------------------------
 
-// Ajouter le produit au ls et ajoute uniquement la quantité si le produit y est déjà
+// Ajouter le produit au local storage et ajouter uniquement la quantité si le produit y est déjà
 /**
- * Ajout de la quantité si le produit est déjà présent dans le localStorage
- * get_article récupère le localStorage et vérifie si le produit choisi est déjà présent dans le ls
+ * Ajout de la quantité si le produit est déjà présent dans le local storage
+ * get_article récupère le local storage et vérifie si le produit choisi est déjà présent dans le ls
  * Si il est déjà présent ajout de de get_article.quantity à product_client.quantity
- * Si le produit n'est pas présent dans le localStorage il l'ajoute directement dans le ls
+ * Si le produit n'est pas présent dans le local storage, il l'ajoute directement dans le ls
  */
 function addLs(product_client) {
   let cart = JSON.parse(localStorage.getItem("product_client")); // JSON.parse permet d'analyser ls.getItem comme du json 
@@ -118,8 +120,8 @@ function addLs(product_client) {
 //------------------------------------------------------------
 
 // Fonction pour les messages d'erreurs
-let newElt = document.createElement("span");
-let elt = color_miss;
+const newElt = document.createElement("span");
+const elt = color_miss;
 elt.appendChild(newElt);
 newElt.classList.add("color__miss", "quantity__miss","product__added", "excess__quantity");
 
@@ -129,7 +131,7 @@ function styleError(){
   newElt.style.textAlign = "center";
   newElt.style.paddingTop = "5px";
 }
-function style(){
+function styleOk(){
   newElt.style.color = "green";
   newElt.style.fontWeight = "bold";
   newElt.style.textAlign = "center";
@@ -137,7 +139,7 @@ function style(){
 }
 
 function colorQuantityMiss(){
-  document.querySelector(".color__miss , .quantity__miss").textContent = "Merci de choisir une couleur et une quantité";
+  document.querySelector(".color__miss , .quantity__miss").textContent = "Merci de choisir une couleur et une quantité comprise entre 1 et 100";
   styleError()
 }
 
@@ -151,12 +153,12 @@ function quantityMiss(){
   styleError()
 }
 
-function productAdded(){
-  document.querySelector(".product__added").textContent = `Votre commande vient d'être ajoutée au panier`;
-  style()
-}
-
 function excessQuantity(){
   document.querySelector(".excess__quantity").textContent = "La quantité totale d'un même article ne peut dépasser 100";
   styleError()
+}
+
+function productAdded(){
+  document.querySelector(".product__added").textContent = `Votre commande vient d'être ajoutée au panier`;
+  styleOk()
 }
